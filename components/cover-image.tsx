083dcf36a59f1943bb64877f4155ec114e767c2e
Jakub Stastka <jakub@stastka.xyz>
2022-12-14 14:34:42 +0100

    transition fix

--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -14,9 +14,9 @@ const CoverImage = ({ title, src, slug }: Props) => {
       src={src}
       alt={`Cover Image for ${title}`}
       className={cn(
-        "shadow-sm w-full rounded-3xl hover:scale-105 transition-transform duration-200",
+        "shadow-sm w-full rounded-3xl hover:scale-105 transition-all duration-200",
         {
-          "hover:shadow-lg transition-shadow duration-200": slug,
+          "hover:shadow-lg transition-shadow": slug,
         }
       )}
       width={1300}
