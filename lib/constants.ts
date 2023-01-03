c08b6f38c5ba3be2be953b5d192a127a98f490a6
Jakub Stastka <jakub@stastka.xyz>
2023-01-03 23:29:02 +0100

    tidying up

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,12 @@
 export const HOME_OG_IMAGE_URL = "/various/home_og_image.png";
+
+// blog name, punchline
 export const BLOG_NAME = "piju kafe";
 export const BLOG_PUNCHLINE = "bez mlíka, bez cukru";
+
+// headers and buttons
 export const KNOW_MORE = "Chcete vědět víc?";
-export const FEATURED_POSTS = "Top články";
 export const I_WANNA_KNOW_MORE = "Chci vědět víc";
+
+export const FEATURED_POSTS = "Top články";
 export const ARCHIVE_ARTICLES = "Starší články";
